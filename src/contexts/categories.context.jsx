14923e405704b07e8211b3d4d39e9a3fb5b48eac
Rytis <r.pleckauskas@gmail.com>
2022-10-29 15:17:30 +0300

    Implemented GraphQL

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,8 +1,9 @@
 import { createContext, useState, useEffect } from 'react';
-import { gql, useQuery } from '@apollo/client/core';
+import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
+  loading: false
 });
 
 const COLLECTIONS = gql`
@@ -24,9 +25,19 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((accu, collection) => {
+        const { title, items } = collection;
+        accu[title.toLowerCase()] = items;
+        return accu;
+      }, {})
+      setCategoriesMap(collectionsMap);
+    }
+  }, [data])
 
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
